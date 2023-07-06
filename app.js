24863a1d1bdf69d4c70620e922fcb051ded29171
osamakhalidali <138747361+osamakhalidali@users.noreply.github.com>
2023-07-06 01:17:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var list =document.getElementById ("list")
+
+
+
+
+
+function Addtodo () {
+var Todo_item = document.getElementById ("Todo_item")
+console.log (Todo_item.value)     
+
+}
+addEventListener,
